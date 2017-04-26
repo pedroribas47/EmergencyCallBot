60d95a3b4e63de37d75ec8724f13966622ee83d8
Pedro Ribas <pedro.ribas@fatec.sp.gov.br>
2017-04-26 10:47:25 -0300

    Arquivos iniciais

---
+++ src/aula/Principal.java
@@ -0,0 +1,133 @@
+package aula;
+
+import java.util.List;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ChatAction;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendChatAction;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import com.pengrad.telegrambot.response.SendResponse;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+public class Principal {
+
+	public static void main(String[] args) {
+
+		// Criação do objeto bot com as informações de acesso
+		TelegramBot bot = TelegramBotAdapter.build("375036253:AAGoNnGFmdS-4SaOP20Y2KgsilrIiLqd3B8");
+
+		// objeto responsável por receber as mensagens
+		GetUpdatesResponse updatesResponse;
+		// objeto responsável por gerenciar o envio de respostas
+		SendResponse sendResponse;
+		// objeto responsável por gerenciar o envio de ações do chat
+		BaseResponse baseResponse;
+
+		// controle de off-set, isto é, a partir deste ID será lido as mensagens
+		// pendentes na fila
+		int m = 0;
+		boolean continuar = true;
+		boolean responde = true;
+		String resp = null;
+		boolean bola = true;
+		// loop infinito pode ser alterado por algum timer de intervalo curto
+		do {
+
+			// executa comando no Telegram para obter as mensagens pendentes a
+			// partir de um off-set (limite inicial)
+			updatesResponse = bot.execute(new GetUpdates().limit(100).offset(m));
+
+			// lista de mensagens
+			List<Update> updates = updatesResponse.updates();
+
+			// análise de cada ação da mensagem
+			for (Update update : updates) {
+
+				// atualização do off-set
+				m = update.updateId() + 1;
+
+				System.out.println("Recebendo mensagem: " + update.message().text());
+				while (bola){
+				// envio de "Escrevendo" antes de enviar a resposta
+				baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+				// verificação de ação de chat foi enviada com sucesso
+				System.out.println("Resposta de Chat Action Enviada? " + baseResponse.isOk());
+				
+				Keyboard keyboard = new ReplyKeyboardMarkup(
+				        new KeyboardButton[]{
+				                new KeyboardButton("Queimaduras"),
+				                new KeyboardButton("Fraturas"),
+				                new KeyboardButton("Sangramentos")
+				        }
+				);
+				
+				sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "olá"));
+				sendResponse = bot.execute(
+						new SendMessage(update.message().chat().id(), "Bem-vindo ao Bot de Primeiros Socorros!"));
+				sendResponse = bot.execute(new SendMessage(update.message().chat().id(),
+				 	"Cod: 1 : Queimaduras\n"
+					+ "Cod: 2 : Fraturas\n"
+					+ "Cod: 3 : Sangramentos\n"
+					+ "Cod: 4 : Envenenamentos\n").replyMarkup(keyboard)
+);
+				
+				
+				 
+				
+
+				
+				bola = false;
+				}
+				 resp = update.message().text();
+				if (resp != null){
+				switch (resp) {
+				
+				
+				case ("Queimaduras"):
+					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "º Isolar a vítima do agente causador do acidente (fogo ou substâncias químicas lesivas, por exemplo, um ácido)\n"
++"º Lavar a área queimada com água corrente limpa\n"
++"º Se houver tecido da vestimenta aderido ao ferimento, este pode ser retirado de forma a não aumentar a lesão, no instante em que se estiver lavando o local\n"
++"º Não colocar água fria, gelo ou sabão sobre o ferimento\n"
++"º Proteja o local com pano e tecido limpo, mantendo a região mais elevada em relação ao resto do corpo para evitar inchaço\n"
++"º Procure um centro de queimadura mais próximo de sua residência."));
+				
+					break;
+				case ("Fraturas"):
+					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "º Se a fratura for exposta, cubra o ferimento com gaze ou pano limpo. Em hipótese alguma tente realinhar o membro ou retornar o osso, isso pode agravar a situação"
++"º Imobilize a região lesada com tábua, papelão ou madeira, envolvendo uma faixa\n"
++"º Em caso de sangramento incessante (hemorragia), realize uma leve compressão com pano limpo."));
+				
+					break;
+				case ("Sangramentos"):
+					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "º Colocar um pano ou papel limpo no ferimento\n"
++"º Fazer pressão sobre o local, o suficiente para deter o sangramento.\n" 
++"º Eleve o braço ou a perna da vítima, mantendo a pressão sobre o ferimento.\n" 
++"º Levar a vítima ao pronto-socorro."));
+	
+					break;
+				}
+				// mudei
+				// do{
+				// if(resp!=null){
+
+				// }
+				// }while(true);
+
+				// envio da mensagem de resposta
+				// sendResponse = bot.execute(new
+				// SendMessage(update.message().chat().id(),"pau"));
+				// verificação de mensagem enviada com sucesso
+				// System.out.println("Mensagem Enviada?" +sendResponse.isOk());
+
+				}
+			}
+		} while (resp != null);
+
+	}
+
+}
